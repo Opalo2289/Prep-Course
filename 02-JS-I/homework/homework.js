0df934beeac8f04b06f81191c5a3baa93c922d2f
Opalo2289 <enrike2206@gmail.com>
2022-07-22 13:10:21 -0500

    lo hice

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "228922";
+const nuevoNum = "35342289";
 
 // Crea una variable booleana:
 const nuevoBool = null;
